ec66351d747536ee82a2d1c63c49d090ae9e9b07
EximiusA0S <luisarochi1@gmail.com>
2025-06-09 21:44:34 -0600

    Paso 3: Separación de variables y escalado

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import kagglehub
 from kagglehub import KaggleDatasetAdapter
 import pandas as pd
 
+from sklearn.preprocessing import StandardScaler
+
 # Cargar el dataset de Kaggle
 df = kagglehub.load_dataset(
     KaggleDatasetAdapter.PANDAS,
@@ -17,3 +19,12 @@ print(df.info())
 
 print("\nDistribución de la variable objetivo:")
 print(df['Class'].value_counts())
+
+
+# Paso 3: Separar variables predictoras y objetivo
+X = df.drop('Class', axis=1)
+y = df['Class']
+
+# Escalar características
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
