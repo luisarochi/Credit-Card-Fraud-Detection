4f903d928fecf26d7a67edabc704a5af12df7604
EximiusA0S <luisarochi1@gmail.com>
2025-06-09 22:08:24 -0600

    Paso 8: entrenar y evaluar modelo con datos balanceados usando SMOTE

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import kagglehub
 from kagglehub import KaggleDatasetAdapter
 import pandas as pd
+import numpy as np
 
 from imblearn.over_sampling import SMOTE
 from sklearn.model_selection import train_test_split
@@ -8,6 +9,18 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import roc_auc_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import (
+    accuracy_score, 
+    precision_score, 
+    recall_score, 
+    f1_score, 
+    confusion_matrix, 
+    roc_auc_score,
+    classification_report
+)
+from imblearn.over_sampling import SMOTE
+
 
 # Cargar el dataset de Kaggle
 df = kagglehub.load_dataset(
